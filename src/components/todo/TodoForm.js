c3f5c3b7d08fb94729356c7a668046998519fd57
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-08-14 18:37:35 -0300

    set proptypes

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -11,7 +11,7 @@ export const TodoForm = (props) => (
   </form>)
 
 
-TodoForm.propTypes = {
+TodoForm.PropTypes = {
   currentTodo: PropTypes.string.isRequired,
   handleInputChange: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired
