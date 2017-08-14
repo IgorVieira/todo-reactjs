abf69d45a1d41b3615d9101466c1ae15a0419e53
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-08-14 13:03:01 -0300

    todo list with react

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm, TodoList} from './components/todo';
+import {addTodo, generateId, findById, toggleTodo, updateTodo} from './lib/todoHelpers.js';
+
+
+class App extends Component {
+  
+  state = {
+    todos:[
+      {id: 1, name: 'Learn JSX', isComplete: true},
+      {id: 2, name: 'Build an Awesome App', isComplete: false},
+      {id: 3, name: 'Skip It', isComplete: false},
+    ],
+    currentTodo: ''
+  }
+
+  handleInputChange = (evt) => {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
+  handleToggle = (id) =>{
+    const todo = findById(id, this.state.todos)
+    const toggled  = toggleTodo(todo)
+    const updateTodos = updateTodo(this.state.todos, toggled)
+    this.setState({todos:updateTodos})
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    const newId = generateId()
+    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+    const updateTodos = addTodo(this.state.todos, newTodo)
+    this.setState({
+      todos: updateTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit = (evt) => {
+    evt.preventDefault()
+
+    this.setState({
+      errorMessage: 'Please type anything'
+    })
+  }
+
+
+  render() {
+    
+    const submitHandler = this.state.currentTodo ?
+                          this.handleSubmit:
+                          this.handleEmptySubmit
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          {this.state.errorMessage && <span className="error">
+            {this.state.errorMessage}  
+          </span>}
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}
+            handleSubmit={submitHandler}/>          
+
+          <TodoList handleToggle={this.handleToggle} todos={this.state.todos}/>
+        
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import {TodoItem} from './TodoItem'
+
+export const TodoList = (props) => {
+    return (
+      <div className="Todo-list">
+          <ul>
+            {props.todos.map(todo => 
+              <TodoItem 
+                handleToggle={props.handleToggle}
+                key={todo.id} {...todo}/>)}
+          </ul>
+      </div>  
+    )
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired
+}
